08e998791da58137c486db96d333c7c9ce6e0a08
sendhelpwebsite <146958844+sendhelpwebsite@users.noreply.github.com>
2024-02-05 19:25:55 +0100

    Add files via upload

---
+++ phishing.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+	<title>php sucks</title>
+	<link rel="stylesheet" href=styles.css" />
+	<style>
+		a{font-size: 30px; font-family: helvetica;}
+	</style>
+</head>
+<body>
+
+<div>
+<?php
+$name=$_POST["username"];
+$name=htmlspecialchars($data);
+if ($name=="kaczka20092") {
+	echo "nice facebook pfp";
+} elseif ($name=="ossisis") {
+	echo "bro can't speak english lmao";
+} elseif ($name=="x10p") {
+	echo "identity theft is a serious crime";
+} else {
+	echo "your details aren't even worth stealing lol"
+}
+?>
+
+<a href="poezja.html" font-size: 30px;>wrócić do poezji</a>
+
+</div>
+
+</body>
+</html>
